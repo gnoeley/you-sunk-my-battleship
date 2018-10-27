cda7fa664176b5538722eaed1a2fd6a4aeb8027c
Andy May <Andy.May@autotrader.co.uk>
2018-10-27 19:28:55 +0100

    gamesession.Session now sends messages to both players when the game state changes

--- sessions/gamesession.py
+++ sessions/gamesession.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 
 from hello.sms.stubSmsSender import sendMessage
+from receive.content_processor import Keyword
 
 class SessionState(Enum):
     STARTING = auto()
@@ -27,38 +28,45 @@ class Session:
         self.player_2_state = PlayerState.NOT_JOINED
 
     def invite_player_2(self):
-        message = self.player_1_num + ' has invited you to a game of Battleships - text ACCEPT to join the game or REJECT to refuse'
-        sendMessage(to=self.player_2_num, text=message)
+        sendMessage(to=self.player_1_num, text=self.player_2_num + ' has been invited')
+        sendMessage(to=self.player_2_num,
+                    text=self.player_1_num + ' has invited you to a game of Battleships - text ' + Keyword.ACCEPT.value + ' to join the game or ' + Keyword.QUIT.value + ' to refuse')
 
         self.player_2_state = PlayerState.INVITED
 
-
     def player_2_accepted_invite(self):
-        message = self.player_2_num + ' has accepted your invite.'
-        sendMessage(to=self.player_1_num, text=message)
+        sendMessage(to=self.player_1_num,
+                    text=self.player_2_num + ' has accepted your invite.' + ' reply ' + Keyword.QUIT.value + ' to quit')
+        sendMessage(to=self.player_2_num,
+                    text='You are now in a game with: ' + self.player_1_num + ' reply ' + Keyword.QUIT.value + ' to quit')
 
         self.player_1_state = PlayerState.IN_GAME
         self.player_2_state = PlayerState.IN_GAME
         self.session_state = SessionState.IN_GAME
 
     def player_2_rejected_invite(self):
-        message = self.player_2_num + ' has rejected your invite :/'
-        sendMessage(to=self.player_1_num, text=message)
+        sendMessage(to=self.player_1_num,
+                    text=self.player_2_num + ' has rejected your invite.' + ' reply ' + Keyword.INVITE.value + ' with a number to invite someone else')
+        sendMessage(to=self.player_2_num,
+                    text='Why so serious?')
 
         self.player_2_state = PlayerState.REJECTED
         self.session_state = SessionState.ENDED
 
     def player_1_quit(self):
-        message = self.player_1_num + ' has quit the game :)'
-        sendMessage(to=self.player_2_num, text=message)
+        sendMessage(to=self.player_1_num,
+                    text='Bye')
+        sendMessage(to=self.player_2_num,
+                    text='Player 1 left')
 
         self.player_1_state = PlayerState.QUIT
         self.session_state = SessionState.ENDED
 
     def player_2_quit(self):
-        message = self.player_2_num + ' has quit the game :)'
-        sendMessage(to=self.player_1_num, text=message)
-
+        sendMessage(to=self.player_1_num,
+                    text='Player 2 left')
+        sendMessage(to=self.player_2_num,
+                    text='Bye')
         self.player_2_state = PlayerState.QUIT
         self.session_state = SessionState.ENDED
 
