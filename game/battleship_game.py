f25fe7d7aeeabdf243381f416286e14f1a3ad188
jenni.foster <jenni.foster@autotrader.co.uk>
2018-10-27 17:23:19 +0100

    can place ships

--- game/battleship_game.py
+++ game/battleship_game.py
@@ -1,3 +1,14 @@
+# Ships
+CARRIER = 5
+BATTLESHIP = 4
+CRUISER = 3
+SUBMARINE = 3
+DESTROYER = 2
+
+# Orientation
+VERTICAL = 0
+HORIZONTAL = 1
+
 def createEmptyBoard():
     return [['e' for i in range(10)] for i in range(10)]
 
@@ -6,3 +17,14 @@ def prettyPrintBoard(board):
         for position in row:
             print(position, end=" ")
         print()
+
+def placeShip(board, ship, orientaion, position):
+    x = position[0]
+    y = position[1]
+
+    if orientaion is VERTICAL:
+        for i in range(ship):
+            board[y+i][x] = 's'
+    else:
+        for i in range(ship):
+            board[y][x+i] = 's'
