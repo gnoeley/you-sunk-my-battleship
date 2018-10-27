6014b75eaa24f4ef5b19b300530098ad46c2d26c
Andy May <Andy.May@autotrader.co.uk>
2018-10-27 21:21:11 +0100

    smsSender now checks if it should send

--- hello/sms/smsSender.py
+++ hello/sms/smsSender.py
@@ -4,14 +4,18 @@ api = clockwork.API(os.environ.get('CLOCKWORK_API_KEY'))
 
 
 def sendMessage(to, text):
-    message = clockwork.SMS(to=to, message=text)
-
-    response = api.send(message)
-
-    if response.success:
-        print (response.id)
+    if os.environ.get('SEND_SMS') != 'True':
+        print('Would have sent "' + text + '" to ' + to)
     else:
-        print (response.error_code)
-        print (response.error_message)
+
+        message = clockwork.SMS(to=to, message=text)
+
+        response = api.send(message)
+
+        if response.success:
+            print(response.id)
+        else:
+            print(response.error_code)
+            print(response.error_message)
 
 
