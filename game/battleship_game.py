e65ba00a4c9eacb311242bd3f29d376a35e6b8ea
jenni.foster <jenni.foster@autotrader.co.uk>
2018-10-27 17:49:43 +0100

    fix all the warning, python n00bs

--- game/battleship_game.py
+++ game/battleship_game.py
@@ -9,25 +9,29 @@ DESTROYER = 2
 VERTICAL = 0
 HORIZONTAL = 1
 
-def createEmptyBoard():
+
+def create_empty_board():
     return [['e' for i in range(10)] for i in range(10)]
 
-def prettyPrintBoard(board):
+
+def pretty_print_board(board):
     for row in board:
         for position in row:
             print(position, end=" ")
         print()
 
-def placeShip(board, ship, orientaion, position):
+
+def place_ship(board, ship, orientation, position):
     initial_x = position[0]
     initial_y = position[1]
 
     for i in range(ship):
-        x = initial_x + i if orientaion is HORIZONTAL else initial_x
-        y = initial_y + i if orientaion is VERTICAL else initial_y
+        x = initial_x + i if orientation is HORIZONTAL else initial_x
+        y = initial_y + i if orientation is VERTICAL else initial_y
         board[y][x] = 's'
 
-def takeFire(board, position):
+
+def take_fire(board, position):
     x = position[0]
     y = position[1]
 
@@ -36,12 +40,13 @@ def takeFire(board, position):
     elif board[y][x] is 'e':
         board[y][x] = 'm'
 
+
 if __name__ == "__main__":
-    board = createEmptyBoard()
-    placeShip(board, CARRIER, VERTICAL, [0, 0])
-    placeShip(board, DESTROYER, HORIZONTAL, [5, 3])
-    prettyPrintBoard(board)
-    takeFire(board, [0, 1])
-    takeFire(board, [9, 9])
+    game_board = create_empty_board()
+    place_ship(game_board, CARRIER, VERTICAL, [0, 0])
+    place_ship(game_board, DESTROYER, HORIZONTAL, [5, 3])
+    pretty_print_board(game_board)
+    take_fire(game_board, [0, 1])
+    take_fire(game_board, [9, 9])
     print()
-    prettyPrintBoard(board)
+    pretty_print_board(game_board)
