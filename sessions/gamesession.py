89a8c64fea63e3c316b3d66147ab1e16bf7f7661
Andy May <Andy.May@autotrader.co.uk>
2018-10-27 20:01:53 +0100

    Methods of gamesession that send messages to players return a string that says what was sent

--- sessions/gamesession.py
+++ sessions/gamesession.py
@@ -29,47 +29,65 @@ class Session:
         self.player_2_state = PlayerState.NOT_JOINED
 
     def invite_player_2(self):
-        sendMessage(to=self.player_1_num, text=self.player_2_num + ' has been invited')
-        sendMessage(to=self.player_2_num,
-                    text=self.player_1_num + ' has invited you to a game of Battleships - text ' + Keyword.ACCEPT.value + ' to join the game or ' + Keyword.QUIT.value + ' to refuse')
+        p1Message = self.player_2_num + ' has been invited'
+        p2Message = self.player_1_num + ' has invited you to a game of Battleships - text ' + Keyword.ACCEPT.value + ' to join the game or ' + Keyword.QUIT.value + ' to refuse'
+
+        sendMessage(to=self.player_1_num, text=p1Message)
+        sendMessage(to=self.player_2_num, text=p2Message)
 
         self.player_2_state = PlayerState.INVITED
 
+        return {'p1': p1Message, 'p2': p2Message}
+
     def player_2_accepted_invite(self):
-        sendMessage(to=self.player_1_num,
-                    text=self.player_2_num + ' has accepted your invite.' + ' reply ' + Keyword.QUIT.value + ' to quit')
-        sendMessage(to=self.player_2_num,
-                    text='You are now in a game with: ' + self.player_1_num + ' reply ' + Keyword.QUIT.value + ' to quit')
+
+        p1Message = self.player_2_num + ' has accepted your invite.' + ' reply ' + Keyword.QUIT.value + ' to quit'
+        p2Message = 'You are now in a game with: ' + self.player_1_num + ' reply ' + Keyword.QUIT.value + ' to quit'
+
+        sendMessage(to=self.player_1_num, text=p1Message)
+        sendMessage(to=self.player_2_num, text=p2Message)
 
         self.player_1_state = PlayerState.IN_GAME
         self.player_2_state = PlayerState.IN_GAME
         self.session_state = SessionState.IN_GAME
 
+        return {'p1': p1Message, 'p2': p2Message}
+
     def player_2_rejected_invite(self):
-        sendMessage(to=self.player_1_num,
-                    text=self.player_2_num + ' has rejected your invite.' + ' reply ' + Keyword.INVITE.value + ' with a number to invite someone else')
-        sendMessage(to=self.player_2_num,
-                    text='Why so serious?')
+        p1Message = self.player_2_num + ' has rejected your invite.' + ' reply ' + Keyword.INVITE.value + ' with a number to invite someone else'
+        p2Message = 'Why so serious?'
+
+        sendMessage(to=self.player_1_num, text=p1Message)
+        sendMessage(to=self.player_2_num, text=p2Message)
 
         self.player_2_state = PlayerState.REJECTED
         self.session_state = SessionState.ENDED
 
+        return {'p1': p1Message, 'p2': p2Message}
+
     def player_1_quit(self):
-        sendMessage(to=self.player_1_num,
-                    text='Bye')
-        sendMessage(to=self.player_2_num,
-                    text='Player 1 left')
+        p1Message = 'Bye'
+        p2Message = 'Player 1 left'
+
+        sendMessage(to=self.player_1_num, text=p1Message)
+        sendMessage(to=self.player_2_num, text=p2Message)
 
         self.player_1_state = PlayerState.QUIT
         self.session_state = SessionState.ENDED
 
+        return {'p1': p1Message, 'p2': p2Message}
+
     def player_2_quit(self):
-        sendMessage(to=self.player_1_num,
-                    text='Player 2 left')
-        sendMessage(to=self.player_2_num,
-                    text='Bye')
+        p1Message = 'Player 2 left'
+        p2Message = 'Bye'
+
+        sendMessage(to=self.player_1_num, text=p1Message)
+        sendMessage(to=self.player_2_num, text=p2Message)
+
         self.player_2_state = PlayerState.QUIT
         self.session_state = SessionState.ENDED
+
+        return {'p1': p1Message, 'p2': p2Message}
 
     def __str__(self) -> str:
         return \
@@ -80,5 +98,3 @@ class Session:
             ", player 1 state:" + self.player_1_state.name + \
             ", player 2 state:" + self.player_2_state.name + \
             "}"
-
-
