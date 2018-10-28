021d50fa426aa3649dd04ae6bf5bc18ab095165f
Andy May <Andy.May@autotrader.co.uk>
2018-10-28 06:27:52 +0000

    xml board sender has column labels

--- send/boardSender.py
+++ send/boardSender.py
@@ -1,9 +1,22 @@
 
 import requests
 import os
+from unicode.symbols import letter_symbols
+
+column_labels = u'\u00a0'.join([
+    letter_symbols.get('A'),
+    letter_symbols.get('B'),
+    letter_symbols.get('C'),
+    letter_symbols.get('D'),
+    letter_symbols.get('E'),
+    letter_symbols.get('F'),
+    letter_symbols.get('G'),
+    letter_symbols.get('H'),
+    letter_symbols.get('I'),
+    letter_symbols.get('J')])
 
 unicodeBoard = \
-    '  A   B   C  D  E   F  H   I\n' + \
+    column_labels + ' \n' + \
     '1◽◽◽◽◽◽◽◽\n' + \
     '2◽◽◽◽◽◽◽◽\n' + \
     '3◽◽◽◽◽◽◽◽\n' + \
@@ -17,7 +30,7 @@ def build_board_xml(aBoard):
            '<Message>' + \
            '<Key>' + os.environ.get('CLOCKWORK_API_KEY') + '</Key>' + \
            '<SMS>' + \
-           '<To>447590433270</To><MsgType>UCS2</MsgType>' + \
+           '<To>447528830422</To><MsgType>UCS2</MsgType>' + \
            '<Content>' + board + '</Content>' + \
            '</SMS>' + \
            '</Message>'
