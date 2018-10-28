a1c922154e62808d55dfb1385682d0316067bdc4
Andy May <Andy.May@autotrader.co.uk>
2018-10-28 12:07:22 +0000

    Hacky fix for circular dependencies - comment out all the things

--- game/event_occurances.py
+++ game/event_occurances.py
@@ -1,18 +1,17 @@
 import random
-from game.battleship_game import game, other_player
-from game.game_entities import Ships
+from game.game_entities import Ships, Players
 
 
 def event_check():
     if random.random() < 0.01:
-        game.winning_player = 'Draw'		# TODO Everyone loses
+        # game.winning_player = 'Draw'		# TODO Everyone loses
         return 'A Kraken rises out of the sea! There were no survivors'
     elif random.random() < 0.03:
         return{
-            game.current_player: 'Pirates have come for your bounty! They plunder your '
-                                 + Ships.name
-                                 + 'and your shipmates are made to walk the plank',
-            other_player(game.current_player): 'The enemy has been attacked by pirates! Their battleship is no longer standing'
+            # game.current_player: 'Pirates have come for your bounty! They plunder your '
+            #                      + Ships.name
+            #                      + 'and your shipmates are made to walk the plank',
+            # Players.other_player(game.current_player): 'The enemy has been attacked by pirates! Their battleship is no longer standing'
         }
     elif random.random() < 0.05:
         return 'A Tsunami hit! Your Submarine has been washed away..'
